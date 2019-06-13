f0817e6dbdb1f4a79804459dedbaa71569c328b6
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-06-13 11:22:41 +0200

    Visual C++ Compatibility: Configuration and Compatibility headers

--- tools/iproxy.c
+++ tools/iproxy.c
@@ -31,12 +31,11 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stddef.h>
-#include <unistd.h>
 #include <errno.h>
 #include <getopt.h>
 #ifdef WIN32
-#include <windows.h>
 #include <winsock2.h>
+#include <windows.h>
 typedef unsigned int socklen_t;
 #else
 #include <sys/socket.h>
